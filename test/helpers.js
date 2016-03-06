b7b86f736054b962e0a1a936fe7cf22395edabda
Dumitru Cantea <canteadumitru@gmail.com>
2016-03-06 17:48:41 +0200

    support metno-symbols

---
+++ test/helpers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var helpers = require('../lib/helpers');
+var assert = require('assert');
+
+describe('helpers', function() {
+	it('should get weather symbol name', function() {
+		var name = helpers.symbolName(1, 'ro');
+		assert.equal('Senin', name);
+	});
+});

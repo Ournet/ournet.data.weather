c90c2a1134bf6fafe9cbfb42e0e4bb8aaef91610
Dumitru Cantea <canteadumitru@gmail.com>
2016-03-06 18:03:07 +0200

    fix symbolName

--- test/helpers.js
+++ test/helpers.js
@@ -5,7 +5,9 @@ var assert = require('assert');
 
 describe('helpers', function() {
 	it('should get weather symbol name', function() {
-		var name = helpers.symbolName(1, 'ro');
+		var name = helpers.symbolName({ number: 1 }, 'ro');
+		assert.equal('Senin', name);
+		name = helpers.symbolName(1, 'ro');
 		assert.equal('Senin', name);
 	});
 });

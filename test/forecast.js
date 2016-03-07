141e2ba54e5cada05675e8204103a021b8c161e8
Dumitru Cantea <canteadumitru@gmail.com>
2016-03-07 22:51:36 +0200

    added forecast sources: yrno & metno

--- test/forecast.js
+++ test/forecast.js
@@ -3,13 +3,24 @@
 var forecast = require('../lib/forecast');
 var assert = require('assert');
 
-var chisinau = { timezone: 'Europe/Chisinau', latitude: 47.0, longitude: 28.8 };
+var chisinau = {
+	name: 'Chisinau',
+	id: 618426,
+	timezone: 'Europe/Chisinau',
+	latitude: 47.0,
+	longitude: 28.8,
+	country_code: 'md',
+	region: { name: 'Chisinau', asciiname: 'Chisinau' },
+	asciiname: 'Chisinau'
+};
 
 describe('forecast', function() {
 	it('should get weather forecast', function() {
-		return forecast.get(forecast.formatSelector(chisinau))
+		var selector = forecast.formatSelector(chisinau);
+		// console.log(selector);
+		return forecast.get(selector)
 			.then(function(report) {
-				// console.log(report.days[1].times);
+				// console.log(report.days);
 				assert.ok(report);
 			});
 	});
